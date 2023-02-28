1775f3f9ae4213745ca9e00eda000c9231393392
Serhii <podajko1997@gmail.com>
2023-02-28 17:30:40 +0200

    Added icon show password in auth page

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,16 +1,24 @@
-import {ChangeEvent} from "react"
+import {ChangeEvent, useState} from "react"
 import {useTranslation} from "react-i18next"
 import Box from "@mui/material/Box"
 import TextField from "@mui/material/TextField"
+import InputAdornment from "@mui/material/InputAdornment"
 import {ReactComponent as LoginBg} from "../../assets/authentication.svg"
 import useAuthContainer from "../../hooks/auth/useAuthContainer"
 import Btn from "../../ui/button/Btn"
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined"
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined"
 
 const Login = () => {
+    const [viewPassword, setViewPassword] = useState<boolean>(false)
     const {userName, userPassword,
         setUserName, setUserPassword, onLogin} = useAuthContainer()
 
     const {t} = useTranslation()
+
+    const renderIconPassword = viewPassword ?
+        <VisibilityOutlinedIcon fontSize="large" onClick={() => setViewPassword(false)} /> :
+        <VisibilityOffOutlinedIcon fontSize="large" onClick={() => setViewPassword(true)} />
 
     return (
         <Box
@@ -50,11 +58,19 @@ const Login = () => {
                 />
 
                 <TextField
+                    type={viewPassword ? "text" : "password"}
                     value={userPassword}
                     onChange={(e: ChangeEvent<HTMLInputElement>) => setUserPassword(e.target.value)}
                     id="standard-basic"
                     label={t("auth.password")}
                     variant="standard"
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="start">
+                                {renderIconPassword}
+                            </InputAdornment>
+                        )
+                    }}
                 />
 
                 <Btn type="contained" onClick={onLogin} sx={{ alignSelf: "start" }}>
