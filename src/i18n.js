e30c9d06af8d0949737b25a1134bdd70710923bc
Serhii <podajko1997@gmail.com>
2023-03-05 14:41:42 +0200

    Fix bug i18n

--- src/i18n.js
+++ src/i18n.js
@@ -18,18 +18,7 @@ i18n
       escapeValue: false,
     },
     backend: {
-      loadPath: () => {
-        console.log(
-          "1 - https://SerhiiPodaiko.github.io/test-project/locales/{{lng}}/translation.json",
-        )
-        console.log(
-          "2 - https://SerhiiPodaiko.github.io/test-project/{{lng}}/translation.json",
-        )
-        console.log("3 - /locales/{{lng}}/translation.json")
-
-        return `https://SerhiiPodaiko.github.io/test-project/locales/{{lng}}/translation.json`
-      },
-      crossDomain: true,
+      loadPath: "/locales/{{lng}}/translation.json",
     },
   })
 
